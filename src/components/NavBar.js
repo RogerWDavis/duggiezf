72a78b52221fc64a8f3c3bf9d92602fe67d2fffd
Roger Davis <roger.davis86@yahoo.com>
2023-11-17 08:35:48 +0000

    updated nvm

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,3 @@
-User
 import styles from "../styles/NavBar.module.css";
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
@@ -26,53 +25,91 @@ const NavBar = () => {
   };
 
   const addPostIcon = (
-    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/posts/create">
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/posts/create"
+    >
       <i className="far fa-plus-square"></i>Add post
     </NavLink>
   );
-
   const loggedInIcons = (
     <>
-      {/* ... (unchanged code) ... */}
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/feed"
+      >
+        <i className="fas fa-stream"></i>Feed
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/liked"
+      >
+        <i className="fas fa-heart"></i>Liked
+      </NavLink>
+      <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+        <i className="fas fa-sign-out-alt"></i>Sign out
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        to={`/profiles/${currentUser?.profile_id}`}
+      >
+        <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+      </NavLink>
     </>
   );
-
+  
   const loggedOutIcons = (
     <>
-      {/* ... (unchanged code) ... */}
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i className="fas fa-sign-in-alt"></i>Sign in
+      </NavLink>
+      <NavLink
+        to="/signup"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <i className="fas fa-user-plus"></i>Sign up
+      </NavLink>
     </>
   );
 
   return (
     <>
-      <Carousel>
+      <Carousel className={`carousel ${styles.carousel}`} indicators={false} controls={false}>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="src/assets/logo.jpg" 
+            src={require("../assets/logo.jpg").default}
             alt="First slide"
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="src/assets/logo.jpg" 
+            src={require("../assets/logo2.jpg").default}
             alt="Second slide"
           />
         </Carousel.Item>
       </Carousel>
-      <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top" style={{ backgroundColor: "transparent", border: "none" }}>
+      <Navbar expanded={expanded} className={styles.navbar} expand="md" fixed="top" style={{ backgroundColor: "transparent", border: "none" }}>
         <Container>
           <NavLink to="/">
-            <Navbar.Brand>
-              <img src={logo} alt="logo" height="45" />
+            <Navbar.Brand className={styles.navbarBrand}>
+              <img src={logo} alt="logo" height="60" className={styles.navbarBrandImg} />
             </Navbar.Brand>
           </NavLink>
           {currentUser && addPostIcon}
           <Navbar.Toggle ref={ref} onClick={() => setExpanded(!expanded)} aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto text-right">
-              <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
+            <Nav className={`ml-auto text-right ${styles.navbarLinkContainer}`}>
+              <NavLink exact className={styles.navbarLink} activeClassName={styles.Active} to="/">
                 <i className="fas fa-home"></i>Home
               </NavLink>
               {currentUser ? loggedInIcons : loggedOutIcons}
@@ -83,3 +120,5 @@ const NavBar = () => {
     </>
   );
 };
+
+export default NavBar;
